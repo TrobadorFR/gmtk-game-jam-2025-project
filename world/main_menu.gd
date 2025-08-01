15d41c3c4e838a197552b96941b34d636c79add6
Ronker123 <3a2f2f2f2f3f3@gmail.com>
2025-08-01 07:29:39 +0000

    main menu developed

--- world/main_menu.gd
+++ world/main_menu.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var world: Node2D = $World
+@onready var selector: CollisionShape2D = $Selector
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	var current_option = selector.ray_cast.get_collider()
+	
+	if selector.ray_cast.is_colliding():
+		if current_option.is_in_group("Play") and Input.is_action_just_pressed("ui_accept"):
+			world.visible = true
+			self.visible = false
+		if current_option.is_in_group("other option") and Input.is_action_just_pressed("ui_accept"):
+			
+			self.visible = false
+		if current_option.is_in_group("some other option") and Input.is_action_just_pressed("ui_accept"):
+			
+			self.visible = false
+		
--- world/selector.gd
+++ world/selector.gd
@@ -1,18 +1,26 @@
 extends CollisionShape2D
 
 
+@onready var options: Node2D = $"../Options"
 @onready var ray_cast: RayCast2D = $RayCast2D
+@onready var option := options.get_children()
+
+@onready var init_opt = $"Play"
+var opt := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	print(option)
+	
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsRayQueryParameters2D.create(Vector2(0, 0), Vector2(50, 100))
-	var result = space_state.intersect_ray(query)
-
-	if result:
-		print(result)
+func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("ui_down"):
+		opt = 0 if (opt + 1) >= option.size() else opt + 1
+	if Input.is_action_just_pressed("ui_up"):
+		opt = option.size()-1 if (opt - 1) < 0 else opt - 1
+	
+	self.global_position = Vector2(self.global_position.x, option[opt].global_position.y)
