26789f38fa49314edc90ab3315c1631329e06df5
denter86@gmail.com <gregoirebrosset17@gmail.com>
2025-08-03 01:55:29 +0200

    Add Interactable stub

--- animals/src/character.gd
+++ animals/src/character.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 @export var GROUND_SPEED = 300.0
 @export var AIR_SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
-@export var RISING_GRAVITY = 980.0
+#@export var RISING_GRAVITY = 980.0
 @export var FALLING_GRAVITY = 980.0
 @export var COYOTE_TIME_DURATION = 0.2
 @export var FASTFALL_GRAVITY_MODIFIER = 2.0
