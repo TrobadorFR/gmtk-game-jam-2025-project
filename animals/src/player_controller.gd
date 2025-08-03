2a71455ea78a1d039075c8fd2d8da055777f994e
denter86@gmail.com <gregoirebrosset17@gmail.com>
2025-08-03 12:20:52 +0200

    Detect closest interactable to playercontroller

--- animals/src/player_controller.gd
+++ animals/src/player_controller.gd
@@ -10,6 +10,11 @@ extends Controller
 var jump_buffer : int = 0
 var interact_buffer : int = 0
 
+## Closest interactable in range. The one that will be targeted for interaction.
+var closest_interactable : Node = null
+
+
+
 func _physics_process(_delta: float) -> void:
 	# IMPORTANT:
 	# I am *fairly certain* that this method causes an issue due to the order of
@@ -35,8 +40,21 @@ func _physics_process(_delta: float) -> void:
 		jump_buffer = max(0, jump_buffer - 1)
 		interact_buffer = max(0, interact_buffer - 1)
 		
+		# Handle interactables
+		var nearby_interactables = $InteractionRange.get_overlapping_areas()
+		if nearby_interactables.size > 0:
+			var new_nearest : Area2D
+			for thing in nearby_interactables:
+				if (position.distance_to(thing.position) < position.distance_to(closest_interactable.position)):
+					new_nearest = thing
+			if new_nearest != closest_interactable:
+				closest_interactable = new_nearest
+				# DISPLAY PROMPT
+		
+		#if interact_buffer and closest_interactable:
+			# DO SHIT
+		
 		if Input.is_action_just_pressed("dbg_switch_char"):
 			#SignalBus.emit_signal("switch_player_character", null)
 			SignalBus.emit_signal("dbg_cycle_characters")
 			# TODO: this must include the reference to the next character later
-		
