42fb416e82db7262928571afc16e48eb895915b1
denter86@gmail.com <gregoirebrosset17@gmail.com>
2025-08-03 18:26:09 +0200

    Add bug audio

--- animals/bug.gd
+++ animals/bug.gd
@@ -8,9 +8,16 @@ func _ready() -> void:
 func _on_change_anim(state: String) -> void:
 	if state == "idle":
 		anim.set_animation("idle")
+		$WalkAudio.stop()
 	elif state == "walking":
 		anim.set_animation("walk")
+		$WalkAudio.play()
+		$JumpAudio.stop()
 	elif state == "rising":
 		anim.set_animation("fly_up")
+		$JumpAudio.play()
+		$WalkAudio.stop()
 	elif state == "falling":
 		anim.set_animation("fly_down")
+		$JumpAudio.play()
+		$WalkAudio.stop()
